f3e35b0a84b0e43cfee218544b2e987a926d62e8
michael <carl.michael.sjogren@gmail.com>
2022-01-21 12:00:11 +0100

    moved customer profile component to customer folder

---
+++ src/components/Customer/CustomerProfile.js
@@ -0,0 +1,34 @@
+﻿import CustomerDetails from "./CustomerDetails";
+import Orders from "./Orders";
+import {useSelector} from "react-redux";
+import React, {Component, useEffect, useState} from "react";
+import { getOne, endpoints } from "../../services/api";
+export const CustomerProfile = () => {
+
+    const Id = useSelector(state => state.user.Id);
+    const [customer, setCustomer] = useState({
+        Id,
+        FullName: "",
+        Email: "",
+        PhoneNumber:"",
+        DefaultShippingAddress:"",
+        BillingAddress:"",
+        Orders: []
+    });
+
+    useEffect(() => {
+        getOne(endpoints.customers, Id).then(data => setCustomer({...data}))        
+    }, [])
+
+    return (
+
+
+            <div >
+                    
+                <CustomerDetails customer={customer} />
+                <Orders orderList={customer.Orders} />       
+            </div>
+    )
+}
+export default CustomerProfile;
+
