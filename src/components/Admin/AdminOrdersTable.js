be1bd6eb8eb087b17177224eaefac0de301563c8
Michael Sjögren <carl.michael.sjogren@gmail.com>
2022-01-24 11:27:56 +0100

    Admin panel (#16)
    * Deleting customers now works
    
    * order deleting
    
    * order form , work in progress
    
    * date now shows on order in admin panel
    
    * showing order details of each order in admin panel.

--- src/components/Admin/AdminOrdersTable.js
+++ src/components/Admin/AdminOrdersTable.js
@@ -1,35 +1,111 @@
 import AdminNav from "../Cart/AdminNav";
-import React, {useEffect, useState} from "react";
-import {endpoints, getAll} from "../../services/api";
+import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { deleteById, endpoints, getAll } from "../../services/api";
+import { Accordion } from 'react-bootstrap';
+import { Button, AccordionBody, AccordionHeader } from "reactstrap";
 
 const AdminOrdersTable = () => {
-    const [orders,setOrders] = useState([])
+    const [orders, setOrders] = useState([])
     useEffect(() => {
         getAll(endpoints.orders).then(data => setOrders(data))
-    },[]);
+    }, []);
+
+    const onHandleDelete = (Id) => {
+        deleteById(endpoints.orders, Id)
+            .then(res => {
+                console.log(res);
+                let index = orders.findIndex(e => e.Id === Id)
+                if (index >= 0) {
+                    let newOrders = [...orders]
+                    newOrders.splice(index, 1,)
+                    setOrders(newOrders)
+                }
+            }
+            )
+    }
     return (
         <div>
-            <AdminNav currentRoute={"orders"}/>
+            <AdminNav currentRoute={"orders"} />
             <h1>Admin Panel - Orders</h1>
             <p>All orders</p>
-            <table className="table table-striped">
-                <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Total Price</th>
-                    <th>Shipping Address</th>
-                    <th>Status</th>
-                </tr>
-                </thead>
-                <tbody>
-                {orders.map(order => <tr key={order.Id}>
-                    <td>{order.Id}</td>
-                    <td>{order.TotalPrice}</td>
-                    <td>{order.ShippingAddress}</td>
-                    <td>{order.OrderStatus}</td>
-                </tr>)}
-                </tbody>
-            </table>
+            <div className="">
+                <div className="row">
+
+                        <div className="col">Order Id</div>
+                        <div className="col">Date Created</div>
+                        <div className="col">Total Price</div>
+                        <div className="col">Status</div>
+                </div>
+                <div>
+                    {orders.map(order => <div key={order.Id}>
+
+                        <Accordion>
+                            <Accordion.Item eventKey="0">
+                                <Accordion.Header className="row">
+                                    <p className="col">
+                                        {order.Id}
+                                    </p>
+
+                                    <p className="col">
+                                        {order.OrderDate}
+                                    </p>
+
+                                    <p className="col">
+                                        ${order.TotalPrice}
+                                    </p>
+
+                                    <p className="col">
+                                        {order.OrderStatus}
+                                    </p>
+                                </Accordion.Header>
+                                <Accordion.Body>
+                                    <p>
+                                    <b>Total Price </b>
+                                         ${order.TotalPrice}
+                                    </p>
+                                    <p>
+                                       <b> Products</b>
+                                    </p>
+                                    { order.Products.map(item => 
+                                    
+                                    <table className="table table-striped" >
+                                        <thead>
+                                            <tr>
+                                            <th>
+                                                Name
+                                            </th>
+                                            <th>
+                                                Price
+                                            </th>
+                                            <th>
+                                                Quantity
+                                            </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    {item.Product.ProductName}
+                                                </td>
+                                                <td>
+                                                    {item.Product.ProductPrice}
+                                                </td>
+                                                <td>
+                                                    {item.Quantity}
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table> )
+
+                                    }
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+
+                    </div>)}
+                </div>
+            </div>
         </div>
     )
 }
