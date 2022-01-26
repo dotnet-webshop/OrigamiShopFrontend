c5e0d2993070f4f5d6a5cf021558e1289eca837e
Michael Sjögren <carl.michael.sjogren@gmail.com>
2022-01-26 14:44:38 +0100

    Validations (#23)
    * Register Validations
    
    * Register Validations
    
    Complete
    
    * Login Validations
    
    Complete
    
    * Create Product form Validations
    
    0 > Stock number < 250
    
    Complete
    
    * Edit Product Validation
    
    * Editing
    
    * product form create validation
    
    * Validations Completed
    
     Register Form
     Login form
     Create Product Form
     Edit Product
    
    Co-authored-by: rababalsurmi <rababalsurmi@gmail.com>

--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -1,10 +1,11 @@
-import {Button, Input, Label} from "reactstrap";
+import {Button, Input, Label, Col} from "reactstrap";
 import {useState} from "react";
 import {Link, Redirect} from "react-router-dom";
 import {login} from '../../services/api';
 import {userActions} from '../../state/actions/index'
 import {bindActionCreators} from "redux";
 import {useDispatch, useSelector} from "react-redux";
+import Form from 'react-bootstrap/Form'
 
 function LoginPage(){
     const [password,setPassword] = useState("")
@@ -14,27 +15,76 @@ function LoginPage(){
     const user = useSelector(state => state.user)
     const dispatch = useDispatch()
     const {onLogin} = bindActionCreators(userActions,dispatch)
+    const [validated, setValidated] = useState(false);
     function  onHandleLogin (e){
         let pw = password
         let mail = email
         setPassword("")
         setEmail("")
+        setValidated(true);
+        e.preventDefault();
+        e.stopPropagation();
+        const form = e.currentTarget;
+        console.log(form.checkValidity())
+        if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation(); 
+            setValidated(true);
+            return
+        }
         login(pw,mail).then(
             res => {
                 if (res !== undefined) {
                     console.log(res.Token)
                     onLogin(res.Token)
+                    setValidated(false);
                 } else {
                     setErrorMessage('Error logging in!')
                 }
             }
         )
     }
-    
+
     const loginForm  = () => (
         <div className="">
             <h2>Login</h2>
             <div>
+                <Form  noValidate validated={validated} onSubmit={onHandleLogin}>
+
+                    <Form.Group as={Col} md="4" controlId="validationCustom02">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            type="email"
+                            aria-describedby="inputGroupPrepend"
+                            value={email} name="email" onChange={(e)=>setEmail(e.target.value)}
+                            required
+                            />
+                            <Form.Control.Feedback type="invalid">
+                            Please provide a valid Email.
+                            </Form.Control.Feedback>
+                    </Form.Group>
+                    <br />
+
+                    <Form.Group as={Col} md="6" controlId="validationCustom06">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            required
+                            value={password}
+                            name="password"
+                            onChange={(e)=>setPassword(e.target.value)} />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid Password.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <br />
+                    <small><Link to="/register">Register account</Link></small>
+                    <h5 className="text-danger">{errorMessage}</h5>
+                    <Button className="btn btn-primary mt-2"  type="submit">Login</Button>
+                    {/* <Button className="btn btn-primary mt-2" onClick={(e) => onHandleLogin(e)} >Login</Button> */}
+                </Form>
+            </div>
+             {/* <div>
                 <Label for="email">Email</Label>
                 <Input value={email} name="email" onChange={(e)=>setEmail(e.target.value)} type={"email"}/>
                 <Label for="password">Password</Label>
@@ -56,11 +106,11 @@ function LoginPage(){
                 <div className="row">
                     <small className="col"> <b> User password</b> virge3d</small>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
     return (
         (!user.isLoggedIn) ? loginForm() : <Redirect to='/'/>
-    )    
+    )
 }
 export default LoginPage;
