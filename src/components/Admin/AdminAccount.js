63289130cd7f07772383542c2c57a705afc9e99d
michael <carl.michael.sjogren@gmail.com>
2022-01-27 13:52:59 +0100

    admin edit form wip

--- src/components/Admin/AdminAccount.js
+++ src/components/Admin/AdminAccount.js
@@ -1,15 +1,38 @@
 import {useSelector} from "react-redux";
 import AdminNav from "../Cart/AdminNav";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import {endpoints,getOne} from '../../services/api';
+import EditAdminForm from "./EditAdminForm";
 const AdminAccount = () => {
     const user = useSelector(state => state.user);
+    const [adminUser,setAdminUser] = useState({
+        Id:"",
+        FullName:"",
+        Email:"",
+        Phone:"",
+    })
+
+    useEffect( () => {
+        getOne(endpoints.customers, user.Id)
+        .then(data => setAdminUser({...data}))
+    }, [])
     return (
         <div>
             <AdminNav currentRoute="account"/>
-            <h1>Admin Panel - Account</h1>
-            <p>Welcome {user.Name}!</p>
-            
+            <article>
+                <h1>Admin Panel - Account</h1>
+                <p>
+                    Welcome <b> {adminUser.FullName}</b>!
+
+                </p>
+                <p>
+                        In the Admin Panel you can edit products, customers, and orders.
+                        You can also change your own information.
+                    </p>
+            </article>
+            <hr/>
+            <h2>Your info</h2>
+            <EditAdminForm adminUser={adminUser}></EditAdminForm>
         </div>
     )
 }
