7932e9e2d788553ca571f06ca5902109bce0990e
rababalsurmi <rababalsurmi@gmail.com>
2022-01-28 10:22:05 +0100

    Update CustomerProfile.js

--- src/components/Customer/CustomerProfile.js
+++ src/components/Customer/CustomerProfile.js
@@ -28,7 +28,7 @@ export const CustomerProfile = () => {
 
 
             <div >
-                <h2 >&emsp;Profile<small className="font-monospace text-muted"> Page </small></h2>
+                <h2 ><small className="font-monospace text-muted">&emsp;Profile Page </small></h2>
                 <CustomerDetails customer={customer} />
                 <br></br>
                 <Orders orderList={customer.Orders} />       
