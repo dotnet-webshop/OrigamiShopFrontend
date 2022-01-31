176f62d3c6607f78937221d23c1fffb3d3225aed
michael <carl.michael.sjogren@gmail.com>
2022-01-31 14:24:05 +0100

    Footer added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,6 +12,18 @@ export class Layout extends Component {
         <Container>
           {this.props.children}
         </Container>
+        <footer className="p-5 mt-5 bg-dark">
+            <div className="">
+            <small className='d-block text-light mx-auto text-center'>&copy; Copyright {new Date(Date.now()).getFullYear()}</small>
+            <ul className="list-inline mx-auto text-center text-light">
+                    <li className="list-inline-item"><a href='https://github.com/KimRoys'>Kim Roysdotter, </a></li>
+                    <li className="list-inline-item"><a href='https://github.com/Nekooos'>Kristoffer, </a></li>
+                    <li className="list-inline-item"><a href='https://github.com/Michael-Sjogren'>Michael Sjögren, </a></li>
+                    <li className="list-inline-item"><a href='https://github.com/Rababalsurmi'>Rabab Alsurmi, </a></li>
+                    <li className="list-inline-item"><a href='https://github.com/nisseka'>Stefan Sundbeck</a></li>
+                </ul>  
+            </div>   
+        </footer>
       </div>
     );
   }
